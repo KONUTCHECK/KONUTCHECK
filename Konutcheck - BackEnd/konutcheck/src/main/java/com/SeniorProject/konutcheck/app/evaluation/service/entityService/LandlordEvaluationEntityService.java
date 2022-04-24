9b61d42fa3e03185b1294f465d182561a632dacd
aytentnbl <17030411012@ybu.edu.tr>
2022-04-24 14:55:54 +0300

    Entity Service added to evaluation package

--- Konutcheck - BackEnd/konutcheck/src/main/java/com/SeniorProject/konutcheck/app/evaluation/service/entityService/LandlordEvaluationEntityService.java
+++ Konutcheck - BackEnd/konutcheck/src/main/java/com/SeniorProject/konutcheck/app/evaluation/service/entityService/LandlordEvaluationEntityService.java
@@ -1,4 +1,13 @@
 package com.SeniorProject.konutcheck.app.evaluation.service.entityService;
 
-public class LandlordEvaluationEntityService {
+import com.SeniorProject.konutcheck.app.evaluation.dao.HomesRelatedWithUsersDao;
+import com.SeniorProject.konutcheck.app.evaluation.dao.LandlordEvaluationDao;
+import com.SeniorProject.konutcheck.app.evaluation.entity.HomesRelatedWithUsers;
+import com.SeniorProject.konutcheck.app.evaluation.entity.LandlordEvaluation;
+import com.SeniorProject.konutcheck.app.general.service.BaseEntityService;
+
+public class LandlordEvaluationEntityService extends BaseEntityService<LandlordEvaluation, LandlordEvaluationDao> {
+    public LandlordEvaluationEntityService(LandlordEvaluationDao dao) {
+        super(dao);
+    }
 }
--- Konutcheck - BackEnd/konutcheck/src/main/java/com/SeniorProject/konutcheck/app/evaluation/service/entityService/TenantEvaluationEntityService.java
+++ Konutcheck - BackEnd/konutcheck/src/main/java/com/SeniorProject/konutcheck/app/evaluation/service/entityService/TenantEvaluationEntityService.java
@@ -1,4 +1,12 @@
 package com.SeniorProject.konutcheck.app.evaluation.service.entityService;
 
-public class TenantEvaluationEntityService {
+import com.SeniorProject.konutcheck.app.evaluation.dao.TenantEvaluationDao;
+import com.SeniorProject.konutcheck.app.evaluation.entity.TenantEvaluation;
+import com.SeniorProject.konutcheck.app.general.service.BaseEntityService;
+
+public class TenantEvaluationEntityService extends BaseEntityService<TenantEvaluation, TenantEvaluationDao> {
+
+    public TenantEvaluationEntityService(TenantEvaluationDao dao) {
+        super(dao);
+    }
 }
